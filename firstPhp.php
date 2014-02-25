90924ba916e1068a611189b5804b0bb4a0c68083
Hien To <ubriela@ubriela.local>
2014-02-24 20:32:39 -0800

    First commit. Adding a README.

--- firstPhp.php
+++ firstPhp.php
@@ -6,40 +6,59 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <title>Google Maps Example</title>
     <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
-
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
     <script type="text/javascript">
         //<![CDATA[
         var map = null;
         function load() {
             map = new google.maps.Map(document.getElementById("map"), {
-                center: new google.maps.LatLng(37.943115,-122.036133),
-                zoom: 9,
+                center: new google.maps.LatLng(37.817712,-122.426834),
+                zoom: 15,
                 mapTypeId: 'roadmap'
+             
+              
             });
-        
-            var infoWindow = new google.maps.InfoWindow;
-
-            // Change this depending on the name of your PHP file             
-            var point = new google.maps.LatLng(47.6145, -122.3418);
-            var html = "Marker's address 12345";
-            var icon = 'http://labs.google.com/ridefinder/images/mm_20_blue.png';
-            /* var marker = new google.maps.Marker({
-                map: map,
-                position: point,
-                icon: icon
+            google.maps.event.addListener(map, 'dblclick', function(event) {
+              
+                var touch_point = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: touch_point,
+                    icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
+                });
+                var infoWindow = new google.maps.InfoWindow;    
+                bindInfoWindow(marker, map, infoWindow, event.latLng.lat()+'-'+ event.latLng.lng());
+                marker.setMap(map);
             });
-            bindInfoWindow(marker, map, infoWindow, html);*/
+      
+           
         }
+        var Array_region = new Array();
+        var Array_polygon = new Array();
+        var polygon = new Array();
+        var point0;
+        var point1;
+        var point2;
+        var point3;
+                     
+        var i = -1;
         var json = "blank";
-        function getJSON(url){
+        
+        function GeoCast_Query(url){
             $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', function(data){
                 json = data.contents;
-                alert('getJSON done');
+             
+                if (json=="blank")
+                    alert(json);
+                else{
+                    obj = JSON.parse(json);
+                    Overlay_GeoCast_Region();
+                }
             });
         }
         
         function add(i){
-            var infoWindow = new google.maps.InfoWindow;           
+         
             var lat = obj.notified_workers.x_cords[i] ;
             var lng = obj.notified_workers.y_cords[i];
             var point = new google.maps.LatLng(lat, lng);
@@ -54,65 +73,73 @@
             i++;
         }
         
-        function addMarker_Interval(){
-            var i = 0;
-            if (i==obj.notified_workers.y_cords.length)
-                clearInterval(myVar);
-            var myVar=setInterval(function(){add(i)},1000);
-            
-            
+     
+        
+       
+        
+       
+        
+       
+        function Overlay_GeoCast_Region(){
+            /*var marker = new google.maps.Marker({
+                map: map,
+                position: new google.maps.LatLng(obj.spatial_task.location[0], obj.spatial_task.location[1]),
+                icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
+            });
+           
+            marker.setMap(map);            */
+            i=-1;
+            var interval = setInterval(function() { 
+                add_geocast_cell(i);
+                i++; 
+                if(i >= obj.geocast_query.x_min_cords.length) 
+                    clearInterval(interval);
+            }, 1000);         
         }
-        function AddPolygon(){
-            var polygon = new Array();
-            if (json=="blank")
-                alert(json);
-            else
-            //    alert("data contained");
-                obj = JSON.parse(json);
-            for (var i = 0; i<6; i++){
+        
+      
+        function add_geocast_cell(i){
+          
                 
-                var lat = obj.notified_workers.x_cords[i] ;
-                var lng = obj.notified_workers.y_cords[i];
-                var point = new google.maps.LatLng(lat, lng);
-                polygon[i] = point;
-            }
-            var endpoint = polygon[polygon.length-1];
-            
-            polygon[polygon.length] = endpoint;
-            var region = new google.maps.Polygon({
-                path:polygon,
+            polygon = new Array();  
+            Array_region = new Array();
+            Array_polygon = new Array();
+                
+            point0 = new google.maps.LatLng(obj.geocast_query.x_min_cords[i],
+            obj.geocast_query.y_min_cords[i]);
+            polygon[0] = point0;
+                
+            point1 = new google.maps.LatLng(obj.geocast_query.x_min_cords[i], 
+            obj.geocast_query.y_max_cords[i]);
+            polygon[1] = point1;
+                
+            point2 = new google.maps.LatLng(obj.geocast_query.x_max_cords[i], 
+            obj.geocast_query.y_max_cords[i]);
+            polygon[2] = point2;
+                
+            point3 = new google.maps.LatLng(obj.geocast_query.x_max_cords[i], 
+            obj.geocast_query.y_min_cords[i]);
+            polygon[3] = point3;
+                
+            polygon[4] = point0;
+                
+            Array_polygon[i]=polygon;
+              
+               
+            Array_region[i] = new google.maps.Polygon({
+                path:Array_polygon[i],
                 strokeColor:"#0000FF",
                 strokeOpacity:0.8,
                 strokeWeight:2,
                 fillColor:"#0000FF",
-                fillOpacity:0.4
+                fillOpacity:0.1
             });
-            region.setMap(map);
-            
-            /* var region;
-
-
-            // Define the LatLng coordinates for the polygon's path.
-            var triangleCoords = [
-                new google.maps.LatLng(37.88956, -121.61944),
-                new google.maps.LatLng(37.89827, -122.06306),
-                new google.maps.LatLng(37.925499, -121.735505),
-                new google.maps.LatLng(37.94752, -121.69631),
-                new google.maps.LatLng(37.88956, -121.61944)
-            ];
-
-            // Construct the polygon.
-            region = new google.maps.Polygon({
-                paths: triangleCoords,
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35
-            });
-
-            region.setMap(map);*/
+            Array_region[i].setMap(map);
+           
         }
+            
+            
+            
        
         function addBLUEMarker(){
             // getJSON('http://137.135.57.140/geocast/37.943115,-122.036133');
@@ -152,26 +179,29 @@
       
     
         
-        function bindInfoWindow(marker, map, infoWindow, html) {
-            google.maps.event.addListener(marker, 'click', function() {
+        function bindInfoWindow(marker, map, infoWindow, html) {    
+            json = "blank";
+            google.maps.event.addListener(marker, 'mouseover', function() {
                 infoWindow.setContent(html);
                 infoWindow.open(map, marker);
             });
+            google.maps.event.addListener(marker, 'mouseout', function() {
+                infoWindow.close(map, marker);
+            });
+            google.maps.event.addListener(marker, 'click', function(event) {
+                url = 'http://geocrowd2.cloudapp.net/geocast/' + event.latLng.lat() + "," + event.latLng.lng();
+                GeoCast_Query(url);
+                alert( 'Lat: ' + event.latLng.lat() + ' and Longitude is: ' + event.latLng.lng() );
+            });
+            
         }
-        
-        function async_simulate(){
-            getJSON('http://137.135.57.140/geocast/37.943115,-122.036133');
-            while (json=='blank') {
-                doNothing();
-            }
-            AddPolygon();
-        }
+       
+       
 
-     
+       
      
 
-        function doNothing() {}
-        
+       
         
 
         //]]>
@@ -181,35 +211,38 @@
 </head>
 
 <body onload="load()">
+    <div style="width: 100%;overflow:auto;">
 
-    <div id="map" style="width: 600px; height: 600px"></div>
+        <div style="float:left;">
+            <input type="button" value="Retrieve JSON" onClick="GeoCast_Query('http://geocrowd2.cloudapp.net/geocast/37.80198523110427,-122.41419297781249')">
+
+            </input>      
+        </div>
+        <div style="float:left;">
+            <div id="map" style="width: 800px; height: 550px"></div>
+        </div>
+    </div>
 
 
-</div>
-<form method="post" > 
 
-    <input type="submit" name="submit" value="Submit"> 
-    <input type="submit" name="null" value="Null"> 
-    <input type="button" value="Retrieve JSON" onClick="getJSON('http://137.135.57.140/geocast/37.943115,-122.036133')"></input>
-    <input type="button" value="Display BLUE Marker" onClick="async_simulate()"></input>    
 
-</form>
-<?php
-$json = "";
 
-if (isset($_POST['submit'])) {
-    $json = file_get_contents('http://137.135.57.140/geocast/37.943115,-122.036133');
-    $obj1 = json_decode($json);
-    $obj2 = $obj1->{'notified_workers'}->{'x_cords'}[1];
-    $obj3 = "false";
-    if ($obj1->{'is_performed'})
-        $obj3 = "true";
-    echo $obj2 . $obj3;
-}
-if (isset($_POST['null'])) {
-    echo "null button is clicked";
-}
-?>
+    <?php
+    $json = "";
+
+    if (isset($_POST['submit'])) {
+        $json = file_get_contents('http://137.135.57.140/geocast/37.80198523110427,-122.41419297781249');
+        $obj1 = json_decode($json);
+        $obj2 = $obj1->{'notified_workers'}->{'x_cords'}[1];
+        $obj3 = "false";
+        if ($obj1->{'is_performed'})
+            $obj3 = "true";
+        echo $obj2 . $obj3;
+    }
+    if (isset($_POST['null'])) {
+        echo "null button is clicked";
+    }
+    ?>
 
 </body>
 
