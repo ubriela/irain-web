5dfbde405871793df086058bdee49d26fcc60865
hto <hto@usc.edu>
2014-03-03 21:05:04 -0800

    s

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/geocast/';
+$config['base_url']	= 'http://geocast.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
