35a81d886583b82eaec3044638b6694b4c5863a4
DKH <dkhung.me@gmail.com>
2014-03-06 09:33:55 +0700

    Fix config base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/GeoCast/geocast/';
+//$config['base_url']	= 'http://localhost/GeoCast/geocast/';
+$config['base_url']	=  'http://geocast.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
