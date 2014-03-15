73dcefad4082928068bcbb3cfd4e1921600c6e4a
hto <hto@usc.edu>
2014-03-15 06:45:52 -0700

    s

--- bsservices/application/controllers/bill.php
+++ bsservices/application/controllers/bill.php
@@ -23,6 +23,7 @@ class Bill extends CI_Controller {
 
     function save_bill() {
         $data = json_decode($this->input->post('mydata'));
+        log_message("debug", var_dump($data, True));
         $success = $this->bill_model->save_bill($data);
         if ($success)
             $this->response ("success", "Saving bill successfully.");
