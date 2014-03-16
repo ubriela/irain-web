47dfe381fe5a4b033c4d0b617029efa696d34d11
hto <hto@usc.edu>
2014-03-15 18:23:57 -0700

    first

---
+++ weathercrowdsource/application/logs/log-2014-03-15.php
@@ -0,0 +1,46 @@
+DEBUG - 2014-03-15 15:54:57 --> Config Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Hooks Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Utf8 Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> UTF-8 Support Enabled
+DEBUG - 2014-03-15 15:54:57 --> URI Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Router Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Output Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Security Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Input Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-03-15 15:54:57 --> Language Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Loader Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Controller Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Helper loaded: form_helper
+DEBUG - 2014-03-15 15:54:57 --> Database Driver Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Model Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Model Class Initialized
+DEBUG - 2014-03-15 15:54:57 --> Helper loaded: url_helper
+ERROR - 2014-03-15 15:54:57 --> here1
+ERROR - 2014-03-15 15:54:57 --> 
+ERROR - 2014-03-15 15:54:57 --> 
+DEBUG - 2014-03-15 15:54:58 --> Final output sent to browser
+DEBUG - 2014-03-15 15:54:58 --> Total execution time: 0.3180
+DEBUG - 2014-03-15 16:03:12 --> Config Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Hooks Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Utf8 Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> UTF-8 Support Enabled
+DEBUG - 2014-03-15 16:03:12 --> URI Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Router Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Output Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Security Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Input Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-03-15 16:03:12 --> Language Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Loader Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Controller Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Helper loaded: form_helper
+DEBUG - 2014-03-15 16:03:12 --> Database Driver Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Model Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Model Class Initialized
+DEBUG - 2014-03-15 16:03:12 --> Helper loaded: url_helper
+ERROR - 2014-03-15 16:03:12 --> here1
+ERROR - 2014-03-15 16:03:12 --> 
+ERROR - 2014-03-15 16:03:12 --> 
+DEBUG - 2014-03-15 16:03:12 --> Final output sent to browser
+DEBUG - 2014-03-15 16:03:12 --> Total execution time: 0.3488
