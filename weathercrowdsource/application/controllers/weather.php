105a03534e284b63f3a3e4ff052d054ebc0d8e98
nghia <nghiacnttqnk34@gmail.com>
2014-04-14 02:50:51 +0700

    report location and report weather at current location

--- weathercrowdsource/application/controllers/weather.php
+++ weathercrowdsource/application/controllers/weather.php
@@ -5,7 +5,6 @@ class Weather extends CI_Controller{
         $this->load->database();
         $this->load->model('user_model');
         $this->load->model('weather_model');
-        $this->load->helper('uuid_helper');
         $this->load->library('session');
         $this->load->helper('json_response');
     }
@@ -13,15 +12,14 @@ class Weather extends CI_Controller{
         $userid = $this->session->userdata('userid');
         $success = false;
         if($userid!=''){
-            $lat = $_GET['lat'];
-            $lng = $_GET['lng'];
-            $code = $_GET['code'];
-            $success = $this->weather_model->insert_location($userid,$lat,$lng,$code);
+            $lat = $_POST['lat'];
+            $lng = $_POST['lng'];
+            $code = $_POST['code'];
+            $time = $_POST['time'];
+            $success = $this->weather_model->insert_location($userid,$lat,$lng,$time);
+            $success = $this->weather_model->insert_weather($userid,$lat,$lng,$code,$time);
         }
         $this->_json_response($success);
-    }
-    public function test(){
-        $this->weather_model->test_insert();
     }
     private function _json_response($data) {
         $this->output->set_content_type('application/json');
--- weathercrowdsource/application/models/weather_model.php
+++ weathercrowdsource/application/models/weather_model.php
@@ -1,33 +1,59 @@
 <?php
 class Weather_model extends CI_Model{
-    public function report_weather(){
-        
-    }
-    public function insert_location($userid,$lat,$lng,$code){
-        
-        $success = false;
-        $location = "'POINT($lat $lng)'";
-        $date_now = date("Y-m-d H:i:s");
-        $active =  $this->user_model->is_active($userid);
-        $taskid = gen_uuid();
-        $this->db->set('workerid', $userid);
-        $this->db->set('location', "GeomFromText($location)", false);
-        $this->db->set('date', $date_now);
-        $this->db->set('isactive', $active);
-        $this->db->insert('location_report');
-        if($this->db->affected_rows()>0){
-            $data_response = array(
-                'taskid' => $taskid,
-                'workerid' => $userid,
-                'response_code' => $code,
-                'response_date' => $date_now,
-                'upload_date' => $date_now
-                );
-            $this->db->insert('responses',$data_response);
-            $success = true;
+    //check 
+    public function is_exits_location($userid){
+        $this->db->where('workerid',$userid);
+        $query = $this->db->get('location_report');
+        if($query->num_rows()>0){
+            return true;
         }
-        return $success;
+        return false;
     }
+    public function insert_location($id,$lat,$lng,$locationtime){
+        $time = strtotime($locationtime);
+        $date = date('Y-m-d H:i:s',$time);
+        $loc = "'POINT($lat $lng)'";
+        $active = $this->user_model->is_active($id);
+        $this->db->set('location', "GeomFromText($loc)",false);
+        $this->db->set('date', $date );
+        $this->db->set('isactive', $active);
+        if($this->is_exits_location($id)){
+            $this->db->where('workerid',$id);
+            $this->db->update('location_report');
+        }else{
+            $this->db->set('workerid',$id);
+            $query = $this->db->insert('location_report');
+        }
+        return true;
+    }
+    public function is_exits_weather($userid,$lat,$lng){
+        $loc = "GeomFromText('POINT($lat $lng)')";
+        $this->db->where('userid',$userid);
+        $this->db->where('location',$loc,false);
+        $query = $this->db->get('weather_location');
+        if($query->num_rows()>0){
+            return true;
+        }
+        return false;
+    }
+    public function insert_weather($id,$lat,$lng,$code,$responsetime){
+        $time = strtotime($responsetime);
+        $date = date('Y-m-d H:i:s',$time);
+        $loc = "'POINT($lat $lng)'";
+        $this->db->set('response_code',$code);
+        $this->db->set('response_date',$date);
+        if($this->is_exits_weather($id,$lat,$lng)){
+            $this->db->where('userid',$id);
+            $this->db->where('location',"GeomFromText($loc)",false);
+            $this->db->update('weather_location');
+        }else{
+            $this->db->set('userid',$id);
+            $this->db->set('location',"GeomFromText($loc)",false);
+            $this->db->insert('weather_location');
+        }
+        return true;
+    }
+    
    
 
     
