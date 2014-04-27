c6164ea2363693b6bed210975333c297b837f192
nghia ho van <nghiacnttqnk34@gmail.com>
2014-04-27 08:16:33 +0000

    geocrowd.php edited online with Bitbucket

--- weathercrowdsource/application/controllers/geocrowd.php
+++ weathercrowdsource/application/controllers/geocrowd.php
@@ -38,7 +38,7 @@ class Geocrowd extends CI_Controller {
         $region_str = "POLYGON((" . $SW_lat . ' ' . $SW_lng . "," . $NE_lat . ' ' . $SW_lng . "," . $NE_lat . ' ' . $NE_lng . "," . $SW_lat . ' ' . $NE_lng . "," . $SW_lat . ' ' . $SW_lng . "))";
 
         $condition = "response_date between '$from' and '$to' and CONTAINS(GeomFromText(\"$region_str\"), GeomFromText(CONCAT('POINT(', x(location), ' ', y(location),')')))";
-        $query = $this->db->select('id, x(location) AS lat, y(location) AS lng, response_code, response_date')->from('weather_location')->where($condition)->order_by('response_date')->get();
+        $query = $this->db->select('id, x(location) AS lat, y(location) AS lng, response_code, response_date')->from('weather_report')->where($condition)->order_by('response_date')->get();
         echo json_encode(array("results" => $query->result()));
     }
 
@@ -46,7 +46,7 @@ class Geocrowd extends CI_Controller {
     function spatiotemporal_query2($SW_lat, $SW_lng, $NE_lat, $NE_lng, $from = '1979-01-01 00:00:00', $to = '2015-01-01 00:00:00') {
 
         $condition = "x(location) >= '$SW_lat' and y(location) >= '$SW_lng' and x(location) <= '$NE_lat' and y(location) <= '$NE_lng' and response_date between '$from' and '$to'";
-        $query = $this->db->select('id, x(location) AS lat, y(location) AS lng, response_code, response_date')->from('weather_location')->where($condition)->order_by('response_date')->get();
+        $query = $this->db->select('id, x(location) AS lat, y(location) AS lng, response_code, response_date')->from('weather_report')->where($condition)->order_by('response_date')->get();
 
         echo json_encode(array("results" => $query->result()));
     }
@@ -61,7 +61,7 @@ class Geocrowd extends CI_Controller {
     function circle_query($lat, $lng, $radius = 100) {
 
         $this->db->select("id, x(location) AS lat, y(location) AS lng, response_code, response_date, (6373000 * acos (cos ( radians( '$lat' ) )* cos( radians( x(location) ) )* cos( radians( y(location) ) - radians( '$lng' ) )+ sin ( radians( '$lat' ) )* sin( radians( x(location) ) ))) AS distance");
-        $this->db->from('weather_location')->order_by('response_date');
+        $this->db->from('weather_report')->order_by('response_date');
         $query = $this->db->having("distance < ' $radius '")->get();
 
         echo json_encode(array("results" => $query->result()));
