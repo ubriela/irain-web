b7ef4e0620a83cff44c7b055a2aa9bca672e0cb7
nghia ho van <nghiacnttqnk34@gmail.com>
2014-06-19 01:52:02 +0000

    database.php edited online with Bitbucket

--- weathercrowdsource/application/config/database.php
+++ weathercrowdsource/application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 //$db['default']['hostname'] = 'localhost';
-//$db['default']['hostname'] = '137.135.59.188';
-$db['default']['hostname'] = '191.236.103.212';
+$db['default']['hostname'] = '137.135.59.188';
+//$db['default']['hostname'] = '191.236.103.212';
 $db['default']['username'] = 'weather';
 $db['default']['password'] = 'weather';
 $db['default']['database'] = 'weathercrowd';
