4b42c8d013c100378ec3c31d7737a0959e13406b
hto <hto@usc.edu>
2014-06-30 23:46:37 -0700

    worker response

--- weathercrowdsource/application/config/config.php
+++ weathercrowdsource/application/config/config.php
@@ -248,14 +248,14 @@ $config['encryption_key'] = 'olivay40cSJZHa22iNhRJWPSDzaqStOn';
 |
 */
 $config['sess_cookie_name']		= 'cisession';
-$config['sess_expiration']		= 60 * 60 * 24 * 265 * 100;
+$config['sess_expiration']		= 60 * 60 * 24 * 365 * 10;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= FALSE;
-$config['sess_time_to_update']	= 60 * 60 * 24 * 265 * 100;
+$config['sess_time_to_update']	= 60 * 60 * 24 * 365 * 10;
 
 /*
 |--------------------------------------------------------------------------
