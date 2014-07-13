3c3b62252ad00c33deb3b262bc49d71cf72588db
nghia ho van <nghiacnttqnk34@gmail.com>
2014-07-13 16:13:57 +0000

    home_view.php edited online with Bitbucket

--- weathercrowdsource/application/views/home_view.php
+++ weathercrowdsource/application/views/home_view.php
@@ -11,6 +11,7 @@
     <!-- JAVASCRIPT -->
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+    <script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerwithlabel/src/markerwithlabel.js"></script>
     <script type="text/javascript" src="<?php echo base_url();?>js/bootstrap.min.js"></script>
     <script src="<?php echo base_url();?>js/function.js"></script>
     <script>
