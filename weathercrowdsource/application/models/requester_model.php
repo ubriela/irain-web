8f823906cf849bbe3d6dd9ce5b60330163e3f966
nghia ho van <nghiacnttqnk34@gmail.com>
2014-08-31 18:18:32 +0000

    requester_model.php edited online with Bitbucket

--- weathercrowdsource/application/models/requester_model.php
+++ weathercrowdsource/application/models/requester_model.php
@@ -58,10 +58,10 @@ class Requester_model extends CI_Model{
             $start = 0;
         }
         $id = $this->session->userdata('userid');
-        $this->db->select('taskid,title, x(location) AS lat, y(location) AS lng,request_date,startdate,enddate, iscompleted');
+        $this->db->select('taskid,title, x(location) AS lat, y(location) AS lng,request_date,startdate,enddate,iscompleted');
         $this->db->from('tasks');
         $this->db->where("requesterid = '$id'");
-        $this->db->order_by('iscompleted desc','taskid desc');
+        $this->db->order_by('iscompleted desc');
         $this->db->limit($number,$start);
         $query = $this->db->get();
         if($query->num_rows()>0){
